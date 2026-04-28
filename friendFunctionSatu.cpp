263967f44edf7e4ec137601bf9bf03ec0b4f2ef1
prinses-racwa <rajwaamelia@gmail.com>
2026-04-28 20:25:54 +0700

    menambahkan prosedur sdan function

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -10,3 +10,15 @@ public:
     friend void setNama (mahasiswa &a, string);
 };
 
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama(joko, "kairi kumar");
+    return 0 ;
+}
